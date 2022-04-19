8a164d6daaf795b4dfb2020ffe2012a31f625e06
Ula <1295593@mail.ru>
2022-04-19 09:48:03 +0800

    Initial Commit

---
+++ ContactListAldoApp/ContactListAldoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListAldoAppApp.swift
+//  ContactListAldoApp
+//
+//  Created by Юлия Алдохина on 19/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ContactListAldoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
